c328fae4884587ca2779c61adda3ead326bab8ba
Siddhant Kumar <62494728+siddhantkumarclasher@users.noreply.github.com>
2020-07-06 09:04:04 +0530

    Add files via upload

---
+++ Polaroid/vendor/imgui/premake5.lua
@@ -0,0 +1,28 @@
+project "imGui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_interval.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:widnows"
+	systemversion "latest"
+	cppdialect "C++17"
+	staticruntime "On"
+
+	filter { "system:windows", "configurations:Release" }
+	buildoptions "/MT"

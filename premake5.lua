cc252e76bba702282b761b1b213b5fb54997f577
Siddhant Kumar <62494728+siddhantkumarclasher@users.noreply.github.com>
2020-07-06 08:56:01 +0530

    Add files via upload

--- premake5.lua
+++ premake5.lua
@@ -13,13 +13,18 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 -- Include directories relative to root folder (solution directory)
 IncludeDir = {}
 IncludeDir["GLFW"] = "Polaroid/vendor/GLFW/include"
+IncludeDir["Glad"] = "Polaroid/vendor/Glad/include"
+IncludeDir["ImGui"] = "Polaroid/vendor/imgui"
 
 include "Polaroid/vendor/GLFW"
+include "Polaroid/vendor/Glad"
+include "Polaroid/vendor/ImGui"
 
 project "Polaroid"
 	location "Polaroid"
 	kind "SharedLib"
 	language "C++"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -37,24 +42,28 @@ project "Polaroid"
 	{
 		"%{prj.name}/src",
 		"%{prj.name}/vendor/spdlog/include",
-		"%{IncludeDir.GLFW}"
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}"
 	}
 
 	links
 	{
 		"GLFW",
+		"Glad",
+		"ImGui",
 		"opengl32.lib"
 	}
 
 	filter "system:windows"
 		cppdialect "C++17"
-		staticruntime "On"
 		systemversion "latest"
 
 		defines
 		{
 			"PL_PLATFORM_WINDOWS",
-			"PL_BUILD_DLL"
+			"PL_BUILD_DLL",
+			"GLFW_INCLUDE_NONE"
 		}
 
 		postbuildcommands
@@ -64,20 +73,24 @@ project "Polaroid"
 
 		filter "configurations:Debug"
 			defines "PL_DEBUG"
+			runtime "Debug"
 			symbols "On"
 
 		filter "configurations:Release"
 			defines "PL_RELEASE"
+			runtime "Release"
 			optimize "On"  
 
 		filter "configurations:Dist"
-			defines "PL_DEBUG"
+			defines "PL_DIST"
+			runtime "Release"
 			optimize "On"
 
 project "Sandbox"
 	location "Sandbox"
 	kind "ConsoleApp"
 	language "C++"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -101,7 +114,6 @@ project "Sandbox"
 
 	filter "system:windows"
 		cppdialect "C++17"
-		staticruntime "On"
 		systemversion "latest"
 
 		defines
@@ -111,12 +123,15 @@ project "Sandbox"
 
 		filter "configurations:Debug"
 			defines "PL_DEBUG"
+			runtime "Debug"
 			symbols "On"
 
 		filter "configurations:Release"
 			defines "PL_RELEASE"
+			runtime "Release"
 			optimize "On"  
 
 		filter "configurations:Dist"
-			defines "PL_DEBUG"
+			defines "PL_DIST"
+			runtime "Release"
 			optimize "On"

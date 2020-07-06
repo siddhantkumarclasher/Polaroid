c0cf4369b8f4e39614979481c18eea28e2914dd5
Siddhant Kumar <62494728+siddhantkumarclasher@users.noreply.github.com>
2020-07-06 08:56:47 +0530

    Add files via upload

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,31 @@
 #include <Polaroid.h>
 
+class ExampleLayer : public Polaroid::Layer
+{
+public:
+	ExampleLayer()
+		:Layer("Example")
+	{
+	}
+
+	void OnUpdate() override
+	{
+		PL_INFO("ExampleLayer::Update");
+	}
+
+	void OnEvent(Polaroid::Event& event) override
+	{
+		PL_TRACE("{0}", event);
+	}
+};
+
 class Sandbox :public Polaroid::Application
 {
 public:
 	Sandbox()
 	{
-
+		PushLayer(new ExampleLayer());
+		PushOverlay(new Polaroid::ImGuiLayer());
 	}
 	~Sandbox()
 	{

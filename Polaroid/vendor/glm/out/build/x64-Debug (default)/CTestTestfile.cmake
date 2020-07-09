682f1f7af44a475f1bbedfd5c0b38bfb817b6143
Siddhant Kumar <62494728+siddhantkumarclasher@users.noreply.github.com>
2020-07-09 19:48:51 +0530

    Add files via upload

---
+++ Polaroid/vendor/glm/out/build/x64-Debug (default)/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/Books/Programs/GameEngine/Polaroid/vendor/glm
+# Build directory: D:/Books/Programs/GameEngine/Polaroid/vendor/glm/out/build/x64-Debug (default)
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
+subdirs("test")
---
+++ Polaroid/vendor/glm/out/build/x64-Debug (default)/test/bug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/Books/Programs/GameEngine/Polaroid/vendor/glm/test/bug
+# Build directory: D:/Books/Programs/GameEngine/Polaroid/vendor/glm/out/build/x64-Debug (default)/test/bug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test-bug_ms_vec_static "D:/Books/Programs/GameEngine/Polaroid/vendor/glm/out/build/x64-Debug (default)/test/bug/test-bug_ms_vec_static.exe")
+set_tests_properties(test-bug_ms_vec_static PROPERTIES  _BACKTRACE_TRIPLES "D:/Books/Programs/GameEngine/Polaroid/vendor/glm/test/CMakeLists.txt;231;add_test;D:/Books/Programs/GameEngine/Polaroid/vendor/glm/test/bug/CMakeLists.txt;1;glmCreateTestGTC;D:/Books/Programs/GameEngine/Polaroid/vendor/glm/test/bug/CMakeLists.txt;0;")

7384c13e40ce0c44899b118c15a3db30c874fdef
Siddhant Kumar <62494728+siddhantkumarclasher@users.noreply.github.com>
2020-07-09 19:38:05 +0530

    Add files via upload

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,152 @@
 workspace "Polaroid"
 	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "Polaroid/vendor/GLFW/include"
+IncludeDir["Glad"] = "Polaroid/vendor/Glad/include"
+IncludeDir["ImGui"] = "Polaroid/vendor/imgui"
+IncludeDir["glm"] = "Polaroid/vendor/glm"
+
+group "Dependencies"
+	include "Polaroid/vendor/GLFW"
+	include "Polaroid/vendor/Glad"
+	include "Polaroid/vendor/ImGui"
+
+group ""
+
+project "Polaroid"
+	location "Polaroid"
+	kind "SharedLib"
+	language "C++"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "plpch.h"
+	pchsource "Polaroid/src/plpch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm",
+		"Polaroid/vendor/glm/glm/**.hpp",
+		"Polaroid/vendor/glm/glm/**.inl"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
+	}
+
+	links
+	{
+		"GLFW",
+		"Glad",
+		"ImGui",
+		"opengl32.lib"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		systemversion "latest"
+
+		defines
+		{
+			"PL_PLATFORM_WINDOWS",
+			"PL_BUILD_DLL",
+			"GLFW_INCLUDE_NONE"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+		}
+
+		filter "configurations:Debug"
+			defines "PL_DEBUG"
+			runtime "Debug"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "PL_RELEASE"
+			runtime "Release"
+			optimize "On"  
+
+		filter "configurations:Dist"
+			defines "PL_DIST"
+			runtime "Release"
+			optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Polaroid/vendor/spdlog/include",
+		"Polaroid/src",
+		"Polaroid/vendor",
+		"%{IncludeDir.glm}"
+	}
+
+	links
+	{
+		"Polaroid"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		systemversion "latest"
+
+		defines
+		{
+			"PL_PLATFORM_WINDOWS"
+		}
+
+		filter "configurations:Debug"
+			defines "PL_DEBUG"
+			runtime "Debug"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "PL_RELEASE"
+			runtime "Release"
+			optimize "On"  
+
+		filter "configurations:Dist"
+			defines "PL_DIST"
+			runtime "Release"
+workspace "Polaroid"
+	architecture "x64"
 
 	configurations
 	{

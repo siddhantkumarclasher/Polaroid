463fccfb7e4c9e882dd88a943f28ce44d7d51e9f
Siddhant Kumar <62494728+siddhantkumarclasher@users.noreply.github.com>
2020-07-09 19:39:32 +0530

    Add files via upload

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,5 +1,7 @@
 #include <Polaroid.h>
 
+#include "imgui/imgui.h"
+
 class ExampleLayer : public Polaroid::Layer
 {
 public:
@@ -10,12 +12,25 @@ public:
 
 	void OnUpdate() override
 	{
-		PL_INFO("ExampleLayer::Update");
+		if (Polaroid::Input::IsKeyPressed(PL_KEY_TAB))
+			PL_TRACE("Tab key is pressed (poll)!");
 	}
 
+	virtual void OnImGuiRender() override
+	{
+		// ImGui::Begin("Test");
+		// ImGui::Text("Hello World");
+		// ImGui::End();
+	}
 	void OnEvent(Polaroid::Event& event) override
 	{
-		PL_TRACE("{0}", event);
+		if (event.GetEventType() == Polaroid::EventType::KeyPressed)
+		{
+			Polaroid::KeyPressedEvent& e = (Polaroid::KeyPressedEvent&)event;
+			if (e.GetKeyCode() == PL_KEY_TAB)
+				PL_TRACE("Tab key is pressed (event)!");
+			PL_TRACE("{0}", (char)e.GetKeyCode());
+		}
 	}
 };
 
@@ -25,7 +40,6 @@ public:
 	Sandbox()
 	{
 		PushLayer(new ExampleLayer());
-		PushOverlay(new Polaroid::ImGuiLayer());
 	}
 	~Sandbox()
 	{
